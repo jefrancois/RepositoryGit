77609922d95577144d3dced8f06aed22db764cae
jefrancois <jacquesedomf@gmail.com>
2020-05-28 22:15:15 -0400

    Tout est ok

--- ProjetGit/src/main/java/edom/com/Calculator.java
+++ ProjetGit/src/main/java/edom/com/Calculator.java
@@ -4,7 +4,12 @@ public class Calculator {
 
 	public int add(int a, int b) {
 		// TODO Auto-generated method stub
-		return 0;
+	    return a + b;
+	}
+	
+	public int mult(int a, int b) {
+		// TODO Auto-generated method stub
+	    return a * b;
 	}
 
 }
