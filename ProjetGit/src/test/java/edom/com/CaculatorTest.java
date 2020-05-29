33cd654dd7358eb8dd8c49fe79e4e25a272968f6
jefrancois <jacquesedomf@gmail.com>
2020-05-28 22:10:17 -0400

    Ajout de la classe Calculator

---
+++ ProjetGit/src/test/java/edom/com/CaculatorTest.java
@@ -0,0 +1,14 @@
+package edom.com;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CaculatorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
