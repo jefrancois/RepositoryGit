824acfe3a616a74df39f0c2e5ab6f219ee8f9227
jefrancois <jacquesedomf@gmail.com>
2020-05-29 01:31:34 -0400

    Ok

--- ProjetGit/src/main/java/edom/com/LancementProgram.java
+++ ProjetGit/src/main/java/edom/com/LancementProgram.java
@@ -3,7 +3,17 @@ package edom.com;
 public class LancementProgram {
 
 	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
 		// TODO Auto-generated method stub
+		Calculator calculator = new Calculator();
+		
+		// Act
+		int somme = calculator.add(a, b);
+		
+		System.out.println(somme);
+		
+		
 
 	}
 
