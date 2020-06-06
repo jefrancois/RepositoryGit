57d41a9359002ed2b2e11b9928c87bfb91dba140
jefrancois <jacquesedomf@gmail.com>
2020-06-06 18:05:57 -0400

    Dernier test

--- ProjetGit/src/main/java/edom/com/Calculator.java
+++ ProjetGit/src/main/java/edom/com/Calculator.java
@@ -8,6 +8,13 @@ public class Calculator {
 	
 	public int mult(int a, int b) {
 		// TODO Auto-generated method stub Ok
+		
+		if (a > b)
+		{a = a;}
+		else
+		{
+			b = b;
+		}
 
 	    return a * b;
 	}
