a48dd4ac4ae052958b6b394d44f74b7e8dd5a4a3
jefrancois <jacquesedomf@gmail.com>
2020-06-06 18:12:08 -0400

    Opopopopo

--- ProjetGit/src/main/java/edom/com/Calculator.java
+++ ProjetGit/src/main/java/edom/com/Calculator.java
@@ -8,13 +8,6 @@ public class Calculator {
 	
 	public int mult(int a, int b) {
 		// TODO Auto-generated method stub Ok
-		
-		if (a > b)
-		{a = a + 0;}
-		else
-		{
-			b = b + 0;
-		}
 
 	    return a * b;
 	}
