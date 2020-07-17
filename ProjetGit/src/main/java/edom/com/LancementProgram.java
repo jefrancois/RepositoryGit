44b736cdae700f2f5741ec61cef0693959e485a6
jefrancois <jacquesedomf@gmail.com>
2020-07-17 01:19:18 -0400

    Un commit de Edom

--- ProjetGit/src/main/java/edom/com/LancementProgram.java
+++ ProjetGit/src/main/java/edom/com/LancementProgram.java
@@ -5,6 +5,16 @@ public class LancementProgram {
 
 	public static void main(String[] args) {		
 		//Dak
+		Integer x;
+		Calculator calcul = new Calculator();
+		
+		x = calcul.add(2, 5);
+		
+		if (x > 8)
+		{System.out.println("Je suis chanceux");}
+		else
+		{System.out.println("Je suis malheureux");}
+		
 	}
 
 }
